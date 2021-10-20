3509c93b7badd2b71a390084eb0b3bc417331923
masa526 <masa526sk8@gmail.com>
2021-10-20 20:41:56 +0900

    fix

--- 3_var/plzfixme/main.go
+++ 3_var/plzfixme/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	WatashiNoHensu = "GYUDON"       //./main.go:6:2: undefined: WatashiNoHensu
+	const WatashiNoHensu = "GYUDON"       //./main.go:6:2: undefined: WatashiNoHensu
 	fmt.Println(WatashiNoHensu)     //./main.go:7:14: undefined: WatashiNoHensu
 }
