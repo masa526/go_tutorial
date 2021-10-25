a4744e7b9a73fabf1a0275298bb198478b735b01
masa526 <masa526sk8@gmail.com>
2021-10-25 22:05:11 +0900

    init

---
+++ 6_struct/weakShop/eaters.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"./shop"
+)
+
+func main() {
+	var name1 string = "GYUDON"
+	if _, err := shop.Eat(name1); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err) //更新
+	}
+
+	var name2 string = ""
+	if _, err := shop.Eat(name2); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err) //更新
+	}
+}
