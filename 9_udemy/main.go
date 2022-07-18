3bc749d19e5c14850ef053280fd42505c063ff33
masa526 <masa526sk8@gmail.com>
2022-07-18 21:12:16 +0900

    型変換

--- 9_udemy/main.go
+++ 9_udemy/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	t, f := true, false
-	fmt.Printf("%T %v %t\n", t, 1, t)
-	fmt.Printf("%T %v %t\n", f, 1, f)
+	var x int = 1
+	xx := float64(x)
 
-	fmt.Println(true && true)
-	fmt.Println(true && false)
-	fmt.Println(false && false)
+	fmt.Printf("%T %v %f \n", xx, xx, xx)
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d \n", yy, yy, yy)
+
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v %d \n", i, i, i)
+
+	var ss string = "14"
+	ii, _ := strconv.Atoi(ss)
+	fmt.Printf("%T %v %d \n", ii, ii, ii)
+
+	h := "hello world"
+	fmt.Println(string(h[0]))
+
 }
